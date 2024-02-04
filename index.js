b79c6e1b5f3aa045eeb44794613223e781a0af07
Parth Rasal <parth.rasal@yahoo.com>
2024-02-04 15:52:49 +0530

    Add grid dimension changeability
    Add a button to change the dimensions of the
    grid as per the requirement of the user.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let mainDiv = document.querySelector(".grid");
 
+let btn = document.querySelector(".btn");
+btn.textContent = "Change Grid Dimensions";
+
 for (let i=0; i<16; i++){
     let divRow = document.createElement("div");
     divRow.setAttribute("class","Row");
@@ -11,16 +14,38 @@ for (let i=0; i<16; i++){
     mainDiv.appendChild(divRow);
 }
 
-let divColAll = document.querySelectorAll(".Col");
+btn.addEventListener('click', () => {
+    let divGrid = document.querySelector(".grid");
+    divGrid.innerHTML = '';
+    let gridDimension = prompt("Enter the grid dimension in terms of grid elements: ");
 
+    for (let i=0; i<gridDimension; i++){
+        let divRow = document.createElement("div");
+        divRow.setAttribute("class","Row");
+        for (let j=0; j<gridDimension; j++){
+            let divCol = document.createElement("div")
+            divCol.setAttribute("class","Col");
+            let size = 960/gridDimension;
+            divCol.setAttribute("style", `width: ${size}px; height: ${size}px`);
+            divRow.appendChild(divCol);
+        }
+        mainDiv.appendChild(divRow);
+    }
+
+    
+    let divColAll = document.querySelectorAll(".Col");
 divColAll.forEach(function(divCol) {
     divCol.addEventListener('mouseover',() => {
-        divCol.setAttribute("style","background-color: red");
+        divCol.setAttribute("style",divCol.getAttribute('style')+";background-color: red");
     });
 });
 
 divColAll.forEach(function(divCol) {
     divCol.addEventListener('mouseout',() => {
-        divCol.setAttribute("style","background-color: white");
+        divCol.setAttribute("style",divCol.getAttribute('style')+";background-color: white");
     });
 });
+
+})
+
+
